8303ebbd7e7c5afdbd642147dc7b833e1ba1424c
Fagner <meurerfagner@hotmail.com>
2020-07-29 12:35:42 -0300

    Mudanças da Aula 2 da Imersão React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={Home} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
